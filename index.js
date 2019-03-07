0d985e7d44969712364be9173cc351640d25b5e5
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-03-07 12:29:37 -0500

    added some functions and HTML

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+/* global $, store */;
+const store = {
+    pageOne: true,
+    pageTwo: false,
+    pageThree: false,
+    pageFour: false
+}
+
+
+const pageOne = function() {
+    //this page will consist of a brief description about me
+}
+
+const pageTwo = function() {
+    //this page will consist of my projects
+}
+
+const pageThree = function() {
+    //this page will consist of my work timeline
+}
+
+const pageFour = function() {
+    //this page will consist of my footer info
+}
+
+function main() {
+    pageOne();
+    pageTwo();
+    pageThree();
+    pageFour();
+}
+
+$(main);
