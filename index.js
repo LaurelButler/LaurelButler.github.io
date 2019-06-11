b14d1dd5ad8b936515d18c241af145a1b1864a56
LaurelButler <laurelbutler126@gmail.com>
2019-06-11 17:33:46 -0400

    added work experience summary

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 /* global $ */;
 
-const landingPage = `<section id="landing-page">
-            <h2> < Tech Stack /> </h2>
+const techSkills = `<section id="tech-stack">
+            <h2 class="tech-title"> < Tech Stack /> </h2>
             <ol>
                 <h3>Front-End</h3>
                 <li>HTML5</li><br>
@@ -28,7 +28,7 @@ const landingPage = `<section id="landing-page">
             <p>&copy;Laurel Butler 2019. All Rights Reserved.</p>
         </footer>`;
 //did not need to declare a function. could put it as a string
-const pageOne = `
+const about = `
             <section id="about-me">
              <header role="banner">
                  <h1 class="name">Laurel Butler</h1> 
@@ -59,7 +59,7 @@ const pageOne = `
             </footer>`;
 
 //did not need to declare a function. could put it as a string
-const pageTwo = 
+const projects = 
             `<section id="projects">
                 <div>
                 <h2 class="work-header">My Homegrown and Made from Scratch Projects</h2>
@@ -74,7 +74,7 @@ const pageTwo =
                 <p>Enjoy a night out on the town with great deals using this application. Search by day of the week to see what's going on in your city.</p>
                 <p>HTML, CSS, JavaScript, React, Node.js, Heroku, PostgreSQL</p>
                 <div class="repo-link">
-                    <a href="https://github.com/LaurelButler/nitelyfe" target="_blank">Repo</a>
+                    <a href="https://github.com/LaurelButler/nitelyfe" target="_blank">Repo Link</a>
                 </div>
                 </div>
                
@@ -90,7 +90,7 @@ const pageTwo =
                 <p>Learn some basic spanish words using this spaced repetition app.</p>
                 <p>HTML, CSS, React, Node.JS, Heroku, PostgreSQL</p>
                 <div class="repo-link">
-                <a href="https://github.com/thinkful-ei-bee/spaced-rep-client-laurel-enrique" target="_blank">Repo</a>
+                <a href="https://github.com/thinkful-ei-bee/spaced-rep-client-laurel-enrique" target="_blank">Repo Link</a>
                 </div>
                 </div>
 
@@ -105,7 +105,7 @@ const pageTwo =
                 <p>A fun quiz to determine how well you know our favorite movie stars.</p>
                 <p>HTML, CSS, JavaScript, jQuery</p>
                 <div class="repo-link">
-                <a href="https://github.com/thinkful-ei-bee/laurel-scottw-quiz-app" target="_blank">Repo</a>
+                <a href="https://github.com/thinkful-ei-bee/laurel-scottw-quiz-app" target="_blank">Repo Link</a>
                 </div>
                 </div>
 
@@ -120,7 +120,7 @@ const pageTwo =
                 <p>Search your favorite characters by name.</p>
                 <p>HTML, React, JSX</p>
                 <div class="repo-link">
-                <a href="https://github.com/thinkful-ei-bee/laurel-star-wars-deploy" target="_blank">Repo</a>
+                <a href="https://github.com/thinkful-ei-bee/laurel-star-wars-deploy" target="_blank">Repo Link</a>
                 </div>
                 </div>
 
@@ -135,7 +135,7 @@ const pageTwo =
                 <p>Search by rating and add bookmarks.</p>
                 <p>HTML, CSS, JavaScript, jQuery</p>
                 <div class="repo-link">
-                <a href="https://github.com/thinkful-ei-bee/Farah-Laurel-Bookmark-app" target="_blank">Repo</a>
+                <a href="https://github.com/thinkful-ei-bee/Farah-Laurel-Bookmark-app" target="_blank">Repo Link</a>
                 </div>
                 </div>
                 </div> 
@@ -147,16 +147,18 @@ const pageTwo =
             </footer>`;
 
 //did not need to declare a function. could put it as a string
-const pageThree = 
+const work = 
     `<section id="work-experience">
     <h1>Work Experience</h1>
+ 
     <div class="work-info">
+        <p>Working as a teacher, I fell in love with challenge. I craved more and moved back to the states to find my next endeavor. I became a server and that taught me all of the life skills necessary to work with every type of individual. I interned at a chiropratic office and was in charged of managing social media, emails, etc. and my curiosty began to blossom with wanting to know how everything worked behind the scenes. This brought me to a pivotal moment in my life and career. I took an interest in tech -- after many networking events and trials and errors on Free Code Camp, I knew web engineering was what I wanted to do. I proceeded to enroll in Thinkful and here we are; me making the best career transition I have ever made.</p>
        <h3><strong>Thinkful Web Development Apprenticeship</strong><br>February 2019 - Present<br>Atlanta, GA</h3> 
        <p>Learned industry best practices and practical software development standards with a focus on HTML5, CSS3, JavaScript, jQuery, Node.js, React, Redux, and algorithms & data structures as well as creating and deploying mobile-first applications while learning new languages and frameworks by collaborating several hours every week with senior web developers.</p>
     </div>
     <div class="work-info">
         <h3><strong>Superica</strong><br>December 2016 - Present<br>Atlanta, GA</h3>
-        <p>Professional support liaison who acted as the first method of contact and aided in product education and experience for the customer. Maintained quality, accuracy and professionalism in a fast-paced environment. Successfully completed the required training programs offered from the company. Ability to multi - task and adapt to changing environment. Perfected the craft of listening and communicating clearly, professionally, and empathetically. Escalated problems or inquiries from customers as needed. Provided a remarkable customer experience. Demonstrated high competency working in a fast - paced, team environment. Motivated colleagues to demonstrate high quality output, and positive work ethic.</p>
+        <p>Professional support liaison who acted as the first method of contact and aided in product education and experience for the customer. Maintained quality, accuracy and professionalism in a fast-paced environment. Successfully completed the required training programs offered from the company. Ability to multi - task and adapt to changing environment. Perfected the craft of listening and communicating clearly, professionally, and empathetically. Escalated problems or inquiries from customers as needed. Demonstrated high competency working in a fast - paced, team environment. Motivated colleagues to demonstrate high quality output, and positive work ethic.</p>
     </div>
     <div class="work-info">
         <h3><strong>Administrative Assistant Internship</strong><br>January 2017 - February 2017<br>Atlanta, GA</h3>
@@ -172,29 +174,30 @@ const pageThree =
         Conducted new student assessments, created quarterly exams, and developed the curriculum based upon American Standards.</p>
     </div>
     <div class="work-info>
-    <h3><strong>Customer Service Representative</strong><br>December 2011 - September 2012<br>Cairo, Egypt</h3>
-    <p> Handled inbound calls in a fast paced environment while diligently taking notes on each customers specific case. I provided a remarkable customer experience while communicating in a courteous manner using approved scripts. I met quality standards along with working collaboratively in a team environment. I educated customers on the company 's procedures while maintaining accuracy and professionalism; mastering empathy.</p>
+        <h3><strong>Customer Service Representative</strong><br>December 2011 - September 2012<br>Cairo, Egypt</h3>
+        <p> Handled inbound calls in a fast paced environment while diligently taking notes on each customers specific case. I provided a remarkable customer experience while communicating in a courteous manner using approved scripts. I met quality standards along with working collaboratively in a team environment. I educated customers on company procedures while maintaining accuracy and professionalism; mastering empathy.</p>
+    </div>
     </section>`;
 
 //this function renders results on the page
 const bindClickHandlers = function() {
-    $('.landing-page').on('click', function (event) {
-        $('#container').html(landingPage);
+    $('.tech-skills-link').on('click', function (event) {
+        $('#container').html(techSkills);
     });
     $('.about-me-link').on('click', function(event){
-        $('#container').html(pageOne);
+        $('#container').html(about);
     });
     $('.projects-link').on('click', function(event){
-        $('#container').html(pageTwo);
+        $('#container').html(projects);
     });
     $('.work-experience-link').on('click', function(event){
-        $('#container').html(pageThree);
+        $('#container').html(work);
     });
 }
        
 //consolidated to one function
 function main() {
     bindClickHandlers();
-    $('#container').html(pageOne); //this makes sure my pageOne is always rendered first
+    $('#container').html(about); //this makes sure my pageOne is always rendered first
 }
 $(main);
